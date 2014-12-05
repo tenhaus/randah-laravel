ab269a2fc8ab6cfaa00033a8e1f546b41aa8818e
Chris Hayen <chayen@gmail.com>
2014-12-05 17:05:26 -0500

    First commit?

---
+++ app/views/landing.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>The Randah Online Gallery</title>
+
+    {{ HTML::style('css/main.css'); }}
+
+    <link href="{{ asset('bower/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <style>
+      @import url(//fonts.googleapis.com/css?family=Oswald:300,400,700);
+    </style>
+
+  </head>
+
+  <body>
+    <div class="container-fluid">
+
+      <div class="row header-container">
+        <div>
+          <h1>The Randah</h1>
+          <h2>Online Art Gallery</h2>
+        </div>
+      </div>
+
+      <div class="row section-header">
+        <h2>Recent</h2>
+        <h3>This is what's new</h3>
+
+      </div>
+
+    </div>
+  </body>
+
+</html>
