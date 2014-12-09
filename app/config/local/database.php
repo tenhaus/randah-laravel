dba01ce18637d823c949e7225060d079592e747a
Chris Hayen <chayen@gmail.com>
2014-12-09 15:51:19 -0500

    Added first migrations

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -20,26 +20,16 @@ return array(
 
 	'connections' => array(
 
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
-
 		'pgsql' => array(
 			'driver'   => 'pgsql',
 			'host'     => 'localhost',
-			'database' => 'homestead',
+			'database' => 'randah',
 			'username' => 'homestead',
 			'password' => 'secret',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
+			'port' 		 => '54320'
 		),
 
 	),
