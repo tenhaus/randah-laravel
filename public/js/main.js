c82e7ad5c70f8c92524151fa5894b5d08dd06e7a
Chris Hayen <chayen@gmail.com>
2014-12-10 15:50:43 -0500

    Playing with tile sizes

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('.header-container').backstretch('/images/header.jpg');
   $(".backstretch > img").css('opacity', .39);
-  $(".image-grid").tilesGallery({"height": '12000'})
+  $(".image-grid").tilesGallery({"height": '16000'})
 });
